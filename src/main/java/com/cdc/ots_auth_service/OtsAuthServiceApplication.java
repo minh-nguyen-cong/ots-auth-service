6ec6f0b4afec5cfdfe8f6f99619934aff74ffed1
Minh Nguyen <91minhnguyen@gmail.com>
2025-09-29 08:26:02 +0700

    Enhance security, refactor code, trigger build 22

--- src/main/java/com/cdc/ots_auth_service/OtsAuthServiceApplication.java
+++ src/main/java/com/cdc/ots_auth_service/OtsAuthServiceApplication.java
@@ -1,25 +1,11 @@
 package com.cdc.ots_auth_service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 public class OtsAuthServiceApplication {
-	private static final Logger LOG = LoggerFactory.getLogger(OtsAuthServiceApplication.class);
-
-	@Value("${spring.datasource.url}")
-	private String datasourceUrl;
-
-	@PostConstruct
-	public void logDbConnectionUrl() {
-		LOG.info("Connecting to DB with JDBC URL: {}", datasourceUrl);
-	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OtsAuthServiceApplication.class, args);
 	}
--- src/main/java/com/cdc/ots_auth_service/controller/AuthController.java
+++ src/main/java/com/cdc/ots_auth_service/controller/AuthController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cdc.ots_auth_service.dto.AuthRequest;
 import com.cdc.ots_auth_service.entity.User;
-import com.cdc.ots_auth_service.repository.UserRepository;
 import com.cdc.ots_auth_service.service.UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,7 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
